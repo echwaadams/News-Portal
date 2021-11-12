fe10c3c2d701b583b5c52157143803f36b14cea0
echwaadams <adamsechwa18@gmail.com>
2021-11-12 10:35:18 +0300

    inherited News class

---
+++ src/main/java/models/DepartmentNews.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.sql.Timestamp;
+
+public class DepartmentNews extends News{
+    private int departmentId;
+
+    public DepartmentNews(int id, int userid, String type, String content, Timestamp postdate, int departmentId) {
+        super(id, userid, type, content, postdate);
+        this.departmentId = departmentId;
+    }
+}
