cedff9ab1cad713e5dae697cba7bd6a6b715722a
echwaadams <adamsechwa18@gmail.com>
2021-11-12 10:42:31 +0300

    overriden constructors

--- src/main/java/models/DepartmentNews.java
+++ src/main/java/models/DepartmentNews.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class DepartmentNews extends News{
     private int departmentId;
@@ -8,5 +9,28 @@ public class DepartmentNews extends News{
     public DepartmentNews(int id, int userid, String type, String content, Timestamp postdate, int departmentId) {
         super(id, userid, type, content, postdate);
         this.departmentId = departmentId;
+    }
+
+    // getter and setter m
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        DepartmentNews that = (DepartmentNews) o;
+        return departmentId == that.departmentId;
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(super.hashCode(), departmentId);
     }
 }
