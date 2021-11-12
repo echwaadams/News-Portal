3fec4e04b61af953226130088df079ab297710b7
echwaadams <adamsechwa18@gmail.com>
2021-11-12 10:32:49 +0300

    add News class

---
+++ src/main/java/models/News.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.sql.Timestamp;
+
+public class News {
+    private int id;
+    private int userid;
+    private String type;
+    private String content;
+    private Timestamp postdate;
+
+    public News(int id, int userid, String type, String content,  Timestamp postdate) {
+        this.id = id;
+        this.userid = userid;
+        this.type = type;
+        this.content = content;
+        this.postdate = postdate;
+    }
+}
