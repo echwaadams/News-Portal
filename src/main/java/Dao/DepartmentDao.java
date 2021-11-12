72fe2c86e13ea850305b565f35e68ce974b9c211
echwaadams <adamsechwa18@gmail.com>
2021-11-12 12:18:37 +0300

    added and implemented department dao

---
+++ src/main/java/Dao/DepartmentDao.java
@@ -0,0 +1,17 @@
+package Dao;
+
+import models.Department;
+import models.DepartmentNews;
+import models.User;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    List<Department> getAllDepartments();
+    List<User> getDepartmentUsersById(int id);
+    List<DepartmentNews> getDepartmentNewsById(int id);
+    void addDepartment(Department department);
+    Department findDepartmentById(int id);
+    void updateDepartment(Department department, String name, String description);
+    void clearAllDepartments();
+}
