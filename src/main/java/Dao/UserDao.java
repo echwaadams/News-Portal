403dd3a0ddfe83620b47965396dcf53068c9abc3
echwaadams <adamsechwa18@gmail.com>
2021-11-12 11:14:08 +0300

    created user interface

--- src/main/java/Dao/UserDao.java
+++ src/main/java/Dao/UserDao.java
@@ -1,2 +1,13 @@
-package Dao;public class UserDao {
+package Dao;
+
+import models.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+    void addUser(User user);
+    User findUserById(int id);
+    void updateUser(User user, String position, String role, int departmentId);
+    void clearAllUsers();
 }
